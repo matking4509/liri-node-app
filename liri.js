4c345193d1fad90703c1b5743f7b74b079bb2279
Mat King <mat@matking.info>
2019-01-29 22:59:51 -0500

    final Submit

--- liri.js
+++ liri.js
@@ -1,25 +1,24 @@
+// Requires
 var dotenv = require("dotenv").config();
 var axios = require("axios");
-var moment = require("moment");
 var Spotify = require('node-spotify-api');
 var keys =  require("./keys");
 var fs = require("fs");
 
+// Define Spotify Objects
 var spotify = new Spotify(keys.spotify);
-var doWhat = process.argv[2];
-var doWho = process.argv[3];
 
+// Decide if main function should use the file for input or the process arguments.
 if (process.argv[2] === "do-what-it-says") {
     fs.readFile("random.txt", "utf8", function(error, data) {
         [doWhat, doWho] = data.split(",");
         main(doWhat, doWho);
     });
 } else {
-    
     main(process.argv[2], process.argv[3]);
 }
 
-
+// Run Main Program.
 function main(what, value) {
     switch (what) {
         case "concert-this":
@@ -33,13 +32,10 @@ function main(what, value) {
         case "movie-this":
         movieThis(value);
         break;
-        
-        // case "do-what-it-says":
-        //   doThis();
-        //   break;
     };
 };
 
+// will search bandsintown.com website for event information based on artist's name and display all events found.
 function concertThis(artist) {
     axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
     .then(function(response) {
@@ -51,6 +47,7 @@ function concertThis(artist) {
     });
 };
 
+// Get Spotiy infomration for a Track this can search and return up to 20 results, so there is a "limit" variable if more than the first result should be returned.  it will loop through all variables and display them.
 function spotifyThis(track) {
     var limit = 1;
     if (!track) { track = "The Sign (US Album)" };
@@ -78,23 +75,12 @@ function spotifyThis(track) {
         });
 };
 
+// Get movie information from OMDB
 function movieThis(title) {
     if (!title) { title = 'Mr. Nobody' };
     axios.get("https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy")
     .then(function(response) {
         response = response.data;
-        // console.log(response.Ratings[1]);
         console.log("\nTitle: " + response.Title + "\nYear: " + response.Year + "\nimdb Rating: " + response.imdbRating + "\nRotten Tomatoes Rating: " + response.Ratings[1].Value + "\nCountry: " + response.Country + "\nLanguage(s): " + response.Language + "\nPlot: " + response.Plot + "\nActors: " + response.Actors) + "\n";
     });
 };
-
-// * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-// * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-// * Edit the text in random.txt to test out the feature for movie-this and concert-this.
-
-// function doThis() {
-//     fs.readFile("random.txt", "utf8", function(error, data) {
-//         [doWhat, doWho] = data.split(",");
-//         console.log("Stuff:", doWhat, doWho);
-//     });
-// };
